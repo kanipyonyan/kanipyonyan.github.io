defab24922603b423e761e530187545cef0f6a6a
kanipyonyan <133233764+kanipyonyan@users.noreply.github.com>
2023-05-27 01:34:27 +0900

    Create con.php

---
+++ co/con.php
@@ -0,0 +1,9 @@
+<form method="POST" action="test2.php">
+    連絡先メールアドレス<br />
+    <input type="text" name="mail_address" value="" /><br /><br />
+    Webネーム<br />
+    <input type="text" name="user_name" value="" /><br /><br />
+    お問い合わせ内容<br />
+    <textarea name=""></textarea><br /><br />
+    <input type="submit" value="問い合わせる" />
+</form>
