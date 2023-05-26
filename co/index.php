e864cf73dc3986d884cde7acb38373cbbda84fa3
kanipyonyan <133233764+kanipyonyan@users.noreply.github.com>
2023-05-27 01:33:48 +0900

    Update index.php

--- co/index.php
+++ co/index.php
@@ -7,15 +7,7 @@
 </head>
 
 <body>
-    <form method="POST" action="test2.php">
-        連絡先メールアドレス<br />
-        <input type="text" name="mail_address" value="" /><br /><br />
-        Webネーム<br />
-        <input type="text" name="user_name" value="" /><br /><br />
-        お問い合わせ内容<br />
-        <textarea name=""></textarea><br /><br />
-        <input type="submit" value="問い合わせる" />
-    </form>
+    <?php require_once "con.php"; ?>
 </body>
 </html>
 
